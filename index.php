e67967e95237edd2f027e53ba6f0046e1597903e
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-10 18:20:39 +0500

    updated

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
 </head>
 
 <body>
-    <div class="main flex w-screen h-screen bg-slate-500 justify-between">
+    <div class="main flex w-screen h-screen bg-slate-500 justify-between ">
         <aside class="left h-screen w-1/4 text-white">
             <div class="h-full w-full  bg-gray-900 rounded-r-2xl flex flex-col items-center pt-10 ">
                 <div class="heading">
                     <h1>Chat</h1>
                 </div>
                 <div class="flex flex-col w-4/5 p-1 ">
-                    <div class="users flex mt-2">
+                    <div class="users flex mt-6">
                         <div class="users-img"> <img class=" w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="" srcset=""></div>
                         <div class="details flex justify-between w-full  ml-4">
                             <div class="username font-semibold ">
@@ -29,8 +29,19 @@
                     </div>
 
                     <!-- example -->
+                    <div class="users flex mt-6">
+                        <div class="users-img"> <img class=" w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="" srcset=""></div>
+                        <div class="details flex justify-between w-full  ml-4">
+                            <div class="username  ">
+                                <span class="block font-bold">Hashir Meraj</span>
 
-                    <div class="users flex mt-4">
+                                <span>This is text</span>
+                            </div>
+                            <div class="time">5:30</div>
+                        </div>
+
+                    </div>
+                    <div class="users flex mt-6">
                         <div class="users-img"> <img class=" w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="" srcset=""></div>
                         <div class="details flex justify-between w-full  ml-4">
                             <div class="username font-semibold ">
@@ -42,35 +53,46 @@
                         </div>
 
                     </div>
-                    <div class="users flex">
-                        <div class="users-img"> <img class=" w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="" srcset=""></div>
-                        <div class="details flex justify-between w-full  ml-4">
-                            <div class="username font-semibold ">
-                                <span class="block font-bold">Hashir Meraj</span>
 
-                                <span>This is text</span>
-                            </div>
-                            <div class="time">5:30</div>
-                        </div>
-
-                    </div>
-                    <div class="users flex">
-                        <div class="users-img"> <img class=" w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="" srcset=""></div>
-                        <div class="details flex justify-between w-full  ml-4">
-                            <div class="username font-semibold ">
-                                <span class="block font-bold">Hashir Meraj</span>
-
-                                <span>This is text</span>
-                            </div>
-                            <div class="time">5:30</div>
-                        </div>
-
-                    </div>
                     <!-- Example End -->
                 </div>
             </div>
         </aside>
-        <div class="right w-[73%] h-screen bg-slate-900 rounded-l-2xl"> </div>
+        <div class="right w-[73%] h-screen bg-slate-900 rounded-l-2xl">
+            <div class="chatarea w-full h-full p-8" id="chatArea">
+                <div class="display-chat w-full  h-full flex flex-col ">
+
+                    <!-- message -->
+                    <div class="message-area flex w-full justify-end">
+                        <div class="users flex  w-2/5 mb-4 ">
+                            <div class="users-img"> <img class=" w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="" srcset=""></div>
+                            <div class="details flex justify-between w-full  ml-4 bg-gray-700 p-2 rounded-b-xl">
+                                <div class="username   text-white">
+                                    <span class="block font-bold">Hashir Meraj</span>
+
+                                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quos atque officia ad necessitatibus. Excepturi ex natus quasi vel officiis.</span>
+                                </div>
+                                <div class="time">5:30</div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- sending message -->
+                    <div class="users flex  w-2/5 mb-4">
+                        <div class="users-img"> <img class=" w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="" srcset=""></div>
+                        <div class="details flex justify-between w-full  ml-4 bg-gray-700 p-2 rounded-b-xl">
+                            <div class="username   text-white">
+                                <span class="block font-bold">Hashir Meraj</span>
+
+                                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quos atque officia ad necessitatibus. Excepturi ex natus quasi vel officiis.</span>
+                            </div>
+                            <div class="time">5:30</div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="./src/script.js"></script>
