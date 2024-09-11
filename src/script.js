74da2a6033b214d2efaca8122aa2bc356054f60c
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-11 18:33:09 +0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,9 +2,7 @@
 
 $(document).ready(function () {
 
-
     var conn = new WebSocket('ws://localhost:8080');
-
     conn.onopen = function (e) {
         console.log("Connection established!");
     };
