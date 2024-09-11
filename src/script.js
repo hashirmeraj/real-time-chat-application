7462f6fe5aca11c7a998ee966366d4d8ccc4cb88
hashirmeraj <hashirmeraj1@gmail.com>
2024-09-11 19:28:30 +0500

    updated

--- src/script.js
+++ src/script.js
@@ -12,9 +12,26 @@ $(document).ready(function () {
         console.log(e.data);
         // Uncomment if you want to display the received message
         var data = JSON.parse(e.data);
-        
-        var html_data = "<div class='sender h-auto w-40 bg-blue-500 mb-2 flex justify-between'> <span> " + data.msg + "</span><span> " + data.uId + "</span></div>";
-        $('#message_area').append(html_data);
+
+        // var html_data = "<div class='sender h-auto w-40 bg-blue-500 mb-2 flex justify-between'> <span> " + data.msg + "</span><span> " + data.uId + "</span></div>";
+        var html_data = `
+    <div class="message-area flex w-full justify-end">
+        <div class="users flex w-2/5 mb-4">
+            <div class="users-img">
+                <img class="w-[60px] h-[50px] rounded-full" src="https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png" alt="">
+            </div>
+            <div class="details flex justify-between w-full ml-4 bg-gray-700 p-2 rounded-b-xl">
+                <div class="username text-white">
+                    <span class="block font-bold">`+ data.userId + `</span>
+                    <span>`+ data.msg + `</span>
+                </div>
+                <div class="time">5:30</div>
+            </div>
+        </div>
+    </div>
+`;
+
+        $('#chatArea').append(html_data);
         $('#chat_message').val('');  // Clear the message input after sending
     };
 
